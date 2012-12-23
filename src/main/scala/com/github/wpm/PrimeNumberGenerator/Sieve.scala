d9aa23b03e4e283f52ee3d3c7d074bcd1165dd1c
W.P. McNeill <billmcn@gmail.com>
2012-12-23 11:30:22 -0800

    Rewrite following the O'Neill paper closely
    A single function using streams enumerating over the multiples with a lazy list.

--- src/main/scala/com/github/wpm/PrimeNumberGenerator/Sieve.scala
+++ src/main/scala/com/github/wpm/PrimeNumberGenerator/Sieve.scala
@@ -1,7 +1,43 @@
 package com.github.wpm.PrimeNumberGenerator
 
-abstract class Sieve {
-  def contains(n: Int): Boolean
+import collection.mutable
 
-  def +(n:Int): Sieve
+object Sieve {
+  def primes(ns: Stream[Int] = skip2357): Stream[Int] = {
+    def primesRec(ns: Stream[Int], q: mutable.PriorityQueue[Stream[Int]]): Stream[Int] = {
+      def smallestComposite = q.head.head
+      val n = ns.head
+      if (n >= smallestComposite) {
+        while (n >= smallestComposite) q += q.dequeue().tail
+        primesRec(ns.tail, q)
+      }
+      else n #:: primesRec(ns.tail, q += ns.map(n * _))
+    }
+    ns.head #:: primesRec(ns.tail, mutable.PriorityQueue(ns.map(ns.head * _))(MultipleOrdering))
+  }
+
+  object MultipleOrdering extends Ordering[Stream[Int]] {
+    def compare(x: Stream[Int], y: Stream[Int]) = y.head.compare(x.head)
+  }
+
+  def wheel(start: Int, steps: Seq[Int]): Stream[Int] = {
+    def wheelRec(n: Int, cycle: Stream[Int]): Stream[Int] = n #:: wheelRec(n + cycle.head, cycle.tail)
+    wheelRec(start, Stream.from(0).flatMap(_ => steps))
+  }
+
+  val skip23 = wheel(5, Seq(2, 4))
+  val skip2357 = wheel(11,
+    Seq(2, 4, 2, 4, 6, 2, 6, 4, 2, 4, 6, 6, 2, 6, 4, 2, 6, 4, 6, 8, 4, 2, 4, 2, 4, 8, 6, 4, 6, 2, 4, 6, 2, 6, 6, 4, 2,
+      4, 6, 2, 6, 4, 2, 4, 2, 10, 2, 10))
+
+  def main(args: Array[String]) {
+    val (fmt, ps) = if (args.isEmpty) ("%d: %d", primes())
+    else {
+      val n = args.head.toInt
+      ("%" + n.toString.length + "d: %d", primes().take(n))
+    }
+    ps.zipWithIndex.foreach {
+      case (p, i) => println(fmt.format(i + 1, p))
+    }
+  }
 }
--- src/test/scala/com/github/wpm/PrimeNumberGenerator/PrimeNumberGeneratorTests.scala
+++ src/test/scala/com/github/wpm/PrimeNumberGenerator/PrimeNumberGeneratorTests.scala
@@ -3,68 +3,32 @@ package com.github.wpm.PrimeNumberGenerator
 import org.scalatest.FlatSpec
 
 class PrimeNumberGeneratorTests extends FlatSpec {
-  behavior of "Multiples"
+  private val take = 1000
 
-  they should "start at the square of a number" in {
-    expect(4)(Multiples(2).head)
-    expect(9)(Multiples(3).head)
-    expect(25)(Multiples(5).head)
+  "The first " + take + " numbers from a wheel skipping 2,3,5 and 7" should "skip those factors" in {
+    expect(None)(compareStreams(Sieve.skip2357, skip(2, 3, 5, 7), take))
   }
 
-  they should "be an increasing sequence of multiples their base" in {
-    expect(List(4, 6, 8, 10))(Multiples(2).take(4).toList)
-    expect(List(9, 12, 15, 18))(Multiples(3).take(4).toList)
-    expect(List(25, 30, 35, 40))(Multiples(5).take(4).toList)
+  "The first " + take + " integers" should "generate primes from 2 on" in {
+    expect(None)(comparePrimeStream(Sieve.primes(Stream.from(2)), 2, take))
   }
 
-  behavior of "Candidates"
-
-  they should "should have the first few elements correct" in {
-    expect(List(2, 3, 4, 5))(new Integers().take(4).toList)
-    expect(List(3, 5, 7, 9))(new OddIntegers().take(4).toList)
-    expect(List(5, 7, 11, 13))(new Skip23().take(4).toList)
+  "The first " + take + " numbers from a wheel skipping 2,3,5, and 7" should "generate primes from 11 on" in {
+    expect(None)(comparePrimeStream(Sieve.primes(), 11, take))
   }
 
-  they should "skip the appropriate factors in their first " + n + " elements" in {
-    expect(None)(new Integers().zip(skip()).take(n).find(p => p._1 != p._2))
-    expect(None)(new OddIntegers().zip(skip(2)).take(n).find(p => p._1 != p._2))
-    expect(None)(new Skip23().zip(skip(2, 3)).take(n).find(p => p._1 != p._2))
-    expect(None)(new Skip2357().zip(skip(2, 3, 5, 7)).take(n).find(p => p._1 != p._2))
+  private def trialDivisionPrimes: Stream[Int] = {
+    def primes(ns: Stream[Int]): Stream[Int] = ns.head #:: primes(ns.tail.filterNot(_ % ns.head == 0))
+    primes(Stream.from(2))
   }
 
-  private def skip(ns: Int*): Iterator[Int] = Iterator.from(2).filterNot(n => ns.exists(n % _ == 0))
+  private def skip(ns: Int*): Stream[Int] = Stream.from(2).filterNot(n => ns.exists(n % _ == 0))
 
-  behavior of "MapSieve"
-
-  it should "always contain the next composite number and not the next prime" in {
-    expect(false)(MapSieve().contains(2))
-    expect(false)((MapSieve() + 2).contains(3))
-    expect(true)((MapSieve() + 2 + 3).contains(4))
-    expect(false)((MapSieve() + 2 + 3 + 4).contains(5))
+  private def comparePrimeStream(primes: Stream[Int], start: Int, take: Int = 1000) = {
+    compareStreams(primes, trialDivisionPrimes.dropWhile(_ < start), take)
   }
 
-  private val n = 1000
-
-  behavior of "MapGenerator"
-
-  it should "produce the same first " + n + " primes as trial division starting at 2" in {
-    expect(None)(MapGenerator().zip(trialDivisionPrimes).take(n).find(p => p._1 != p._2))
-  }
-
-  behavior of "QueueGenerator"
-
-  it should "produce the same first " + n + " primes as trial division starting at the appropriate number" in {
-    expect(None)(QueueGenerator(new Integers).zip(trialDivisionPrimes).take(n).find(p => p._1 != p._2))
-    expect(None)(QueueGenerator(new OddIntegers).zip(trialDivisionPrimes.dropWhile(_ < 3)).take(n).find(p => p._1 != p._2))
-    expect(None)(QueueGenerator(new Skip23).zip(trialDivisionPrimes.dropWhile(_ < 3)).take(n).find(p => p._1 != p._2))
-    expect(None)(QueueGenerator().zip(trialDivisionPrimes.dropWhile(_ < 11)).take(n).find(p => p._1 != p._2))
-  }
-
-  private def trialDivisionPrimes: Iterator[Int] = {
-    def primes(ns: Iterator[Int]): Iterator[Int] = {
-      val n = ns.next()
-      Iterator(n) ++ primes(ns.filterNot(_ % n == 0))
-    }
-    primes(Iterator.from(2))
+  private def compareStreams(actual: Stream[Int], expect: Stream[Int], take: Int) = {
+    expect.zip(actual).take(take).find(p => p._1 != p._2)
   }
 }
